6d5a46843442eb6b2e8d26d383bc99b1bd39c76b
Arthur Chretien <achretie@1G3.42angouleme.fr>
2023-08-15 19:45:58 +0200

    All exceptions done

--- Irc.cpp
+++ Irc.cpp
@@ -37,7 +37,138 @@ void Server::Join()
     
 }
 
+void Server::PrivMsg()
+{
+    
+}
+
 void Server::checkCommand(int sd, char *buffer)
 {
     std::cout << "Received data from client, socket fd: " << sd << ", Data: " << buffer << std::endl;
+}
+
+//ERROR_MSGS
+const char *Server::ERR_NEEDMOREPARAMS::what() const throw()
+{
+    return ("Need more params");
+}
+const char *Server::ERR_ALREADYREGISTRED::what() const throw()
+{
+    return("Already registered");
+}
+const char *Server::ERR_NONICKNAMEGIVEN::what() const throw()
+{
+    return("No nickname given");
+}
+const char *Server::ERR_ERRONEUSNICKNAME::what() const throw()
+{
+    return("Erroneous nickname");
+}
+const char *Server::ERR_NICKNAMEINUSE::what() const throw()
+{
+    return("Nickname is already in use");
+}
+const char *Server::ERR_NICKCOLLISION::what() const throw()
+{
+    return("Nickname collision KILL");
+}
+const char *Server::ERR_NOTONCHANNEL::what() const throw()
+{
+    return("You're not on that channel");
+}
+const char *Server::ERR_NOSUCHCHANNEL::what() const throw()
+{
+    return("No such channel");
+}
+const char *Server::ERR_NOSUCHNICK::what() const throw()
+{
+    return("No such nick/channel");
+}
+const char *Server::ERR_CHANOPRIVSNEEDED::what() const throw()
+{
+    return("You're not channel operator");
+}
+const char *Server::ERR_USERONCHANNEL::what() const throw()
+{
+    return("<user> <channel> :is already on channel");
+}
+const char *Server::RPL_INVITING::what() const throw()
+{
+    return("<channel> <nick>");
+}
+const char *Server::RPL_AWAY::what() const throw()
+{
+    return("<nick> :<away message>");
+}
+const char *Server::RPL_TOPIC::what() const throw()
+{
+    return("<channel> :<topic>");
+}
+const char *Server::RPL_NOTOPIC::what() const throw()
+{
+    return("<channel> :No topic is set");
+}
+const char *Server::ERR_UNKNOWNMODE::what() const throw()
+{
+    return("<char> :is unknown mode char to me");
+}
+const char *Server::ERR_USERSDONTMATCH::what() const throw()
+{
+    return(":Cant change mode for other users");
+}
+const char *Server::ERR_UMODEUNKNOWNFLAG::what() const throw()
+{
+    return(":Unknown MODE flag");
+}
+const char *Server::ERR_KEYSET::what() const throw()
+{
+    return("<channel> :Channel key already set");
+}
+const char *Server::RPL_UMODEIS::what() const throw()
+{
+    return("<user mode string>");
+}
+const char *Server::RPL_BANLIST::what() const throw()
+{
+    return("<channel> <banid>");
+}
+const char *Server::RPL_CHANNELMODEIS::what() const throw()
+{
+    return("<channel> <mode> <mode params>");
+}
+const char *Server::RPL_ENDOFBANLIST::what() const throw()
+{
+    return("<channel> :End of channel ban list");
+}
+const char *Server::ERR_NORECIPIENT::what() const throw()
+{
+    return("No recipient given (<command>)");
+}
+const char *Server::ERR_CANNOTSENDTOCHAN::what() const throw()
+{
+    return("<channel name> :Cannot send to channel");
+}
+const char *Server::ERR_WILDTOPLEVEL::what() const throw()
+{
+    return("<mask> :Wildcard in toplevel domain");
+}
+const char *Server::ERR_NOTEXTTOSEND::what() const throw()
+{
+    return(":No text to send");
+}
+const char *Server::ERR_NOTOPLEVEL::what() const throw()
+{
+    return("<mask> :No toplevel domain specified");
+}
+const char *Server::RPL_SUMMONING::what() const throw()
+{
+    return("<user> :Summoning user to IRC");
+}
+const char *Server::ERR_NOLOGIN::what() const throw()
+{
+    return("<user> :User not logged in");
+}
+const char *Server::ERR_TOOMANYTARGET::what() const throw()
+{
+    return("<target> :Duplicate recipients. No message delivered");
 }
--- Irc.hpp
+++ Irc.hpp
@@ -14,103 +14,137 @@ class Server
         void    Topic();
         void    Mode();
         void    Join();
+        void    PrivMsg();
         void    checkCommand(int sd, char *buffer);
-        // class ERR_NEEDMOREPARAMS : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_ALREADYREGISTRED : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_NONICKNAMEGIVEN : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_ERRONEUSNICKNAME : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_NICKNAMEINUSE : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_NICKCOLLISION : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_BADCHANMASK : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_NOTONCHANNEL : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_NOSUCHCHANNEL : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_NOSUCHNICK : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_CHANOPRIVSNEEDED : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_USERONCHANNEL : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class RPL_INVITING : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class RPL_AWAY : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class RPL_TOPIC : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class RPL_NOTOPIC : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_UNKNOWNMODE : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_USERSDONTMATCH : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_UMODEUNKNOWNFLAG : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class ERR_KEYSET : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class RPL_UMODEIS : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class RPL_BANLIST : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class RPL_CHANNELMODEIS : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
-        // class RPL_ENDOFBANLIST : public std::exception
-        // {
-        //     virtual const char* what() const throw();
-        // };
+        class ERR_NEEDMOREPARAMS : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_ALREADYREGISTRED : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NONICKNAMEGIVEN : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_ERRONEUSNICKNAME : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NICKNAMEINUSE : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NICKCOLLISION : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_BADCHANMASK : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NOTONCHANNEL : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NOSUCHCHANNEL : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NOSUCHNICK : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_CHANOPRIVSNEEDED : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_USERONCHANNEL : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_INVITING : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_AWAY : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_TOPIC : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_NOTOPIC : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_UNKNOWNMODE : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_USERSDONTMATCH : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_UMODEUNKNOWNFLAG : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_KEYSET : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_UMODEIS : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_BANLIST : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_CHANNELMODEIS : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_ENDOFBANLIST : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NORECIPIENT : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_CANNOTSENDTOCHAN : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_WILDTOPLEVEL : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NOTEXTTOSEND : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NOTOPLEVEL : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class RPL_SUMMONING : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_NOLOGIN : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+        class ERR_TOOMANYTARGET : public std::exception
+        {
+            virtual const char* what() const throw();
+        };
+
 };
 
 #endif
